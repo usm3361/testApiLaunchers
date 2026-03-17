9e6d5ccb73fea2e1b7e534ce30d08e4b2c8f9ee0
usm3361 <usm3361@gmail.com>
2026-03-17 17:56:19 +0200

    create login page

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,34 @@
 import { useNavigate } from "react-router";
-import {useAuthStore}
-
+import { useAuthStore } from "../store/authStore";
+import { useState } from "react";
 
 export default function LoginPage() {
-    const login = useAuthStore((s)=>s.login)
-    const nav = useNavigate()
+  const login = useAuthStore((s) => s.login);
+  const nav = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    await login(form);
+    nav("/");
+  };
   return (
-    <div>
-      
+    <div className="form">
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button className="button-login" onClick={submit}>
+        Login
+      </button>
     </div>
-  )
+  );
 }
